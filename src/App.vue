8862569db505e9b4e06c082b5b255f31e93212e6
phphe <phphe@outlook.com>
2020-03-08 16:57:16 +0800

    add Vuetify demo

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,112 @@
 <template lang="pug">
 #app
-  Row(style="background: teal;" :smGutterX="5" :xlGutterX="16" :smGutterY="5" :xlGutterY="16")
-    Col(:width="1/5" v-for="n in total" :key="n+10")
-      .box.red
-  button(@click="total++") add
-  button(@click="total--") reduce
-  hr
-  Row
-    Col(:width="1/24" v-for="n in 24" :key="n+100")
-      .box.red
-    Col(:width="0.5")
-      Row
-        Col(:width="0.5")
-          .box.red
-        Col(:width="0.5")
-          .box.red
-    Col(:width="0.5")
-      .box.red
-    Col(:width="0.5")
-      .box.yellow
-    Col(:width="0.2" xsGrow)
-      .box.red Grow xs
-    BreakRow(xs)
-    Col(:width="0.2" xsGrow)
-      .box.red Grow xs
-    BreakRow
-    Col(grow)
-      .box.red
-    Col(:width="0.2")
-      .box.red
-    BreakRow
-    Col(grow)
-      .box.red
-    BreakRow
-    Col
-      h2 2
-    Col(:width="0.2")
-      .box.red
-    Col(:width="0.2")
-      .box.red
-      hr
-      .box.red
-    Col(:xs="0.5")
-      .box.red
-    Col(:width="0.5")
-      .box.red
-    Col(:width="0.5")
-      .box.red
-    Col(:width="0.5")
-      .box.yellow
-    Col(:width="0.5")
-      hr
-    Col(width="100px")
-      hr
-    Col(:width="1")
-      .box.red
-    Col
-      .box.yellow
-    Col(grow)
-      .box.red
-    BreakRow
-    Col(grow)
-      .box.red
-    Col
-      hr
-    Col(v-for="i in 10" :key="i" width="300" grow)
-      .box.red
-    Col(:width="0.6")
-      div
+  .pvs
+    button.mybtn(@click="vuetify=!vuetify") Toggle VuetifyDemo
+  VuetifyDemo(v-if="vuetify")
+  template(v-else)
+    Row(style="background: teal;" :smGutterX="5" :xlGutterX="16" :smGutterY="5" :xlGutterY="16")
+      Col(:width="1/5" v-for="n in total" :key="n+10")
+        .box.red
+    .pvs
+      button.mybtn(@click="total++") add
+      button.mybtn(@click="total--") reduce
+    Row
+      Col(:width="1/24" v-for="n in 24" :key="n+100")
+        .box.red
+      Col(:width="0.5")
         Row
           Col(:width="0.5")
-            Row
-              Col(:width="0.5")
-                .box.yellow
-              Col(:width="0.5")
-                .box.yellow
+            .box.red
           Col(:width="0.5")
-            .box.yellow
-    Col(:width="0.5")
-      .box.red
-    Col(:width="50")
-      .box.red
-    Col(grow)
-      .box.red
-    BreakRow
-    Col(:width="50")
-      .box.red
-    Col(:width="200")
-      .box.red
-    Col(:width="2" grow)
-      .box.red
-    
-
+            .box.red
+      Col(:width="0.5")
+        .box.red
+      Col(:width="0.5")
+        .box.yellow
+      Col(:width="0.2" xsGrow)
+        .box.red Grow xs
+      BreakRow(xs)
+      Col(:width="0.2" xsGrow)
+        .box.red Grow xs
+      BreakRow
+      Col(grow)
+        .box.red
+      Col(:width="0.2")
+        .box.red
+      BreakRow
+      Col(grow)
+        .box.red
+      BreakRow
+      Col
+        h2 2
+      Col(:width="0.2")
+        .box.red
+      Col(:width="0.2")
+        .box.red
+        hr
+        .box.red
+      Col(:xs="0.5")
+        .box.red
+      Col(:width="0.5")
+        .box.red
+      Col(:width="0.5")
+        .box.red
+      Col(:width="0.5")
+        .box.yellow
+      Col(:width="0.5")
+        hr
+      Col(width="100px")
+        hr
+      Col(:width="1")
+        .box.red
+      Col
+        .box.yellow
+      Col(grow)
+        .box.red
+      BreakRow
+      Col(grow)
+        .box.red
+      Col
+        hr
+      Col(v-for="i in 10" :key="i" width="300" grow)
+        .box.red
+      Col(:width="0.6")
+        div
+          Row
+            Col(:width="0.5")
+              Row
+                Col(:width="0.5")
+                  .box.yellow
+                Col(:width="0.5")
+                  .box.yellow
+            Col(:width="0.5")
+              .box.yellow
+      Col(:width="0.5")
+        .box.red
+      Col(:width="50")
+        .box.red
+      Col(grow)
+        .box.red
+      BreakRow
+      Col(:width="50")
+        .box.red
+      Col(:width="200")
+        .box.red
+      Col(:width="2" grow)
+        .box.red
 </template>
 
 <script>
+import VuetifyDemo from './dev/VuetifyDemo'
 import Row from './components/Row.vue'
 import Col from './components/Col.vue'
 import BreakRow from './components/BreakRow.vue'
 export default {
-  components: {Row, Col, BreakRow},
+  components: {Row, Col, BreakRow, VuetifyDemo},
   data() {
     return {
       total: 21,
+      vuetify: false,
     }
   },
   // computed: {},
@@ -124,5 +128,31 @@ export default {
 .yellow{
   background: yellow;
 }
-
+.mybtn {
+	box-shadow: 0px 1px 0px 0px #f0f7fa;
+	background:linear-gradient(to bottom, #33bdef 5%, #019ad2 100%);
+	background-color:#33bdef;
+	border-radius:6px;
+	border:1px solid #057fd0;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px -1px 0px #5b6178;
+}
+.mybtn:hover {
+	background:linear-gradient(to bottom, #019ad2 5%, #33bdef 100%);
+	background-color:#019ad2;
+}
+.mybtn:active {
+	position:relative;
+	top:1px;
+}
+.pvs{
+  padding: 5px 0;
+}
 </style>
---
+++ src/dev/VuetifyDemo.vue
@@ -0,0 +1,79 @@
+<template lang="html">
+  <div class="vuetify-demo">
+    <Row>
+      <Col :width="300"><Card1 /></Col>
+      <Col grow :xs="1">
+        <Row>
+          <Col :width="1/3" :xs="1/2">
+            <label>First Name</label><v-text-field solo></v-text-field>
+          </Col>
+          <Col :width="1/3" :xs="1/2">
+            <label>Last Name</label><v-text-field solo></v-text-field>
+          </Col>
+          <Col :width="1/3" :xs="1">
+            <label>Email</label><v-text-field solo></v-text-field>
+          </Col>
+          <Col :width="3/5" :xs="1">
+            <label>Website</label><v-text-field solo></v-text-field>
+          </Col>
+          <Col :width="2/5" :xs="1">
+            <label>Title</label><v-text-field solo></v-text-field>
+          </Col>
+          <Col>
+            <label>About</label><v-textarea solo></v-textarea>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
+    <!-- 2 -->
+    <Row>
+      <Col :xl="1/5" :lg="1/4" :md="1/3" :sm="1/2" :xs="1" v-for="i in 23" :key="i+'card'"><Card1 /></Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+import "./vuetify";
+import Row from "../components/Row.vue";
+import Col from "../components/Col.vue";
+import BreakRow from "../components/BreakRow.vue";
+
+const Card1 = {
+  template: `
+    <v-card class="mx-auto">
+      <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+
+      <v-card-title>Top western road trips</v-card-title>
+
+      <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+    </v-card>
+  `
+}
+
+export default {
+  components: { Row, Col, BreakRow, Card1 }
+  // props: {},
+  // data() {
+  //   return {}
+  // },
+  // computed: {},
+  // watch: {},
+  // methods: {},
+  // created() {},
+  // mounted() {}
+};
+</script>
+
+<style lang="scss">
+.vuetify-demo {
+  max-width: 1100px;
+  margin: 0 auto;
+  border: 1px solid #ccc;
+  padding: 20px;
+  background: #e4e4e4;
+  label{
+    display: block;
+    margin-bottom: 10px;
+  }
+}
+</style>
